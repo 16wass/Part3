802ccfb33968e985f5f79430e99198f7a9b23b0f
wassim mezghanni <ge27tuv@mytum.de>
2024-04-16 20:51:45 +0200

    Course information step7

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 const Header = ({ course }) => <h1>{course}</h1>;
@@ -9,21 +10,29 @@ const Part = ({ part }) => (
 );
 
 const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
+    <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
   </>
+    
+
 );
 
 const Course = ({ course }) => {
-  
+  const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0);
 
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      
+      <p>Total number of exercises: {totalExercises}</p>
     </div>
   );
 };
